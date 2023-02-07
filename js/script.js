fe68744cdef8df25e143fe28e74b3d6f5626ab92
Aleksandr <141289united@gmail.com>
2023-02-07 17:44:30 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict"
+
+let number = 5;
+const leftBorderWidth = 1;
+
+console.log(arr.[1]);
+console.log(arrObj[])
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(--decr);
+
+console.log(4 * 3 ** 2);
